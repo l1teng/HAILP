d78336d63e3909c757050f17d9ec44a5026938e4
Teng Li <tengli.terry@gmail.com>
2021-06-19 18:38:58 +0800

    enhance: public index update script

--- docs/public/update_readme.py
+++ docs/public/update_readme.py
@@ -71,11 +71,53 @@ def update_software_index(fold_path, base_url):
         f.write(software_index)
 
 
-def update_hfut_courses_index():
-    pass
+def update_hfut_courses_index(fold_path, base_url):
+    EXCLUDE_FILES = ['gen-md.py', 'readme.md', 'README.md', 'update.bat', 'LICENSE', 'mkdocs.yml']
+    BIN_URL_PREFIX = 'https://172.18.220.5/_public/hfut_courses/'
+
+    def gen_course_md(course_path):
+        cur_course_index = '## File Browser of {}\n'.format(course_path.split('/')[-1])
+        course_f_cats = glob.glob(course_path + '/*')
+        for item in course_f_cats:
+            if os.path.isfile(item):
+                cur_course_index += '\n- [**{}**]({})\n'.format(item.split('/')[-1], base_url + course_path.split('/')[-1] + '/' + item.split('/')[-1])
+        for item in course_f_cats:
+            if os.path.isdir(item):
+                cur_course_index += '\n### {}\n'.format(item.split('/')[-1])
+                cur_cat_f_list = glob.glob(item + '/*')
+                for cur_file in cur_cat_f_list:
+                    cur_course_index += '\n- [**{}**]({})\n'.format(cur_file.split('/')[-1], base_url + course_path.split('/')[-1] + '/' + item.split('/')[-1] + '/' + cur_file.split('/')[-1])
+                    # print('\n- [**{}**]({})\n'.format(cur_file.split('/')[-1], base_url + course_path.split('/')[-1] + '/' + item.split('/')[-1] + '/' + cur_file.split('/')[-1]))
+        
+        return cur_course_index
+    
+    courses_list = glob.glob(fold_path + '/*')
+    courses_main_page_index = '## File Browser of {}\n'.format(fold_path.split('/')[-1])
+    for cur_course in courses_list:
+        if cur_course.split('/')[-1] not in EXCLUDE_FILES:
+            print(cur_course.split('/')[-1])
+            courses_main_page_index += '\n- [**{}**]({})\n'.format(cur_course.split('/')[-1], md_base_url + 'hfut_courses/' + cur_course.split('/')[-1])
+    # print(courses_main_page_index)
+    with open(workspace_path + 'hfut_courses.md', 'w') as f:
+        f.write(courses_main_page_index)
+        
+    courses_index_dict = {}
+    for cur_course in courses_list:
+        if os.path.isdir(cur_course):
+            courses_index_dict[cur_course.split('/')[-1]] = gen_course_md(cur_course)
+    for (cur_courses_k, cur_courses_v) in courses_index_dict.items():
+        # print(workspace_path + 'hfut_courses/' + cur_courses_k + '.md')
+        with open(workspace_path + 'hfut_courses/' + cur_courses_k + '.md', 'w') as f:
+            f.write(cur_courses_v)
+
+
+    
+    
+
 
 if __name__ == '__main__':
     update_public_index(public_path)
     update_dataset_index(public_path + 'datasets/', base_url + 'datasets/')
     update_books_index(public_path + 'books/', base_url + 'books/')
     update_software_index(public_path + 'softwares/', base_url + 'softwares/')
+    update_hfut_courses_index(public_path + 'hfut_courses/', base_url + 'hfut_courses/')
